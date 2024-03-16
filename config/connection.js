a81a4717f8ec93b203ead9b18926a59746457540
yashraj <yashraj.s2711@gmail.com>
2024-03-16 17:55:54 +0530

    add atlas URL

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,7 @@ async function connectDB() {
             dbName : "HACKME"
         }
         await mongoose.connect("mongodb://0.0.0.0:27017/HACKME",options);
+        // await mongoose.connect("mongodb+srv://<username>:<password>@cluster0.ugnodjt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",options);
         console.log("Successfully connected to the database.");
     } catch (error) {
         console.log("Unable to connect with the database:", error.message);
