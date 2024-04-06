b6efa56f5770bf77c0e9ad63d873e0d5a2134c20
yashraj <yashraj.s2711@gmail.com>
2024-04-06 22:38:13 +0530

    add env file

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,20 @@
+require('dotenv').config()
 const mongoose = require("mongoose");
+
 async function connectDB() {
     try {
         mongoose.set('strictQuery', true);
         const options = {
             dbName : "HACKME"
         }
-        await mongoose.connect("mongodb://0.0.0.0:27017/HACKME",options);
+        await mongoose.connect(process.env.BASEURL_MONGO,options);
         // await mongoose.connect("mongodb+srv://<username>:<password>@cluster0.ugnodjt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",options);
         console.log("Successfully connected to the database.");
     } catch (error) {
         console.log("Unable to connect with the database:", error.message);
     }
 }
+
+console.log(process.env.BASEURL_MONGO)
 
 module.exports = connectDB;
